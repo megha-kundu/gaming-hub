752aa237cee4054f320e0b6d44831b4a7d94f28d
MeghaKundu <meghakundumeghakundu@gmail.com>
2026-08-30 15:26:56 +0530

    Fix production question API URL

--- src/api/questionsApi.js
+++ src/api/questionsApi.js
@@ -1,6 +1,6 @@
 export const fetchQuestions = async (category, level) => {
     const res = await fetch(
-        `http://localhost:5000/api/questions?category=${category}&level=${level}`
+        `/api/questions?category=${category}&level=${level}`
     );
 
     const data = await res.json();
